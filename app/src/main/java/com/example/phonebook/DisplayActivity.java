91be4f621c96afe3ad70376d8546b580bafca982
Spencer Parton <srparton1@buffs.wtamu.edu>
2022-04-06 17:53:20 -0500

    log statement update

--- app/src/main/java/com/example/phonebook/DisplayActivity.java
+++ app/src/main/java/com/example/phonebook/DisplayActivity.java
@@ -21,13 +21,13 @@ public class DisplayActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d("Display", "In Display Activity");
         setContentView(R.layout.activity_display);
         String [] projection = {MyContentProvider.COLUMN_FNAME,
                 MyContentProvider.COLUMN_LNAME,MyContentProvider.COLUMN_PHONENUM};
         Cursor cursor = getContentResolver().query(MyContentProvider.CONTENT_URI,projection,
                 null,null);
         int count = cursor.getCount();
+        Log.d("count", "Record Number "+count);
         cursor.moveToFirst();
         TableLayout tableLayout = new TableLayout(this);
         tableLayout.setVerticalScrollBarEnabled(true);
@@ -41,47 +41,47 @@ public class DisplayActivity extends AppCompatActivity {
         t0.setPadding(20,20,20,20);
         tableRow.addView(t0);
 
-        t1 = new TextView(this);
-        t1.setText("Last Name");
-        t1.setTextColor(Color.RED);
-        t1.setTypeface(null, Typeface.BOLD);
-        t1.setPadding(20,20,20,20);
-        tableRow.addView(t1);
-
-        t2 = new TextView(this);
-        t2.setText("Phone Number");
-        t2.setTextColor(Color.RED);
-        t2.setTypeface(null, Typeface.BOLD);
-        t2.setPadding(20,20,20,20);
-        tableRow.addView(t2);
-//        Log.d("Display", "tableRow: "+tableRow);
-
+//        t1 = new TextView(this);
+//        t1.setText("Last Name");
+//        t1.setTextColor(Color.RED);
+//        t1.setTypeface(null, Typeface.BOLD);
+//        t1.setPadding(20,20,20,20);
+//        tableRow.addView(t1);
+//
+//        t2 = new TextView(this);
+//        t2.setText("Phone Number");
+//        t2.setTextColor(Color.RED);
+//        t2.setTypeface(null, Typeface.BOLD);
+//        t2.setPadding(20,20,20,20);
+//        tableRow.addView(t2);
+////        Log.d("Display", "tableRow: "+tableRow);
+//
         tableLayout.addView(tableRow);
-        for (int i = 0; i < count; i++) {
-            Log.d("Display", "here");
-
-            tableRow = new TableRow(this);
-            t0 = new TextView(this);
-            t0.setText(cursor.getString(cursor.getColumnIndex("fName")));
-            t0.setTextColor(Color.BLACK);
-            t0.setPadding(20,20,20,20);
-            tableRow.addView(t0);
-
-            t1 = new TextView(this);
-            t1.setText(cursor.getString(cursor.getColumnIndex("lName")));
-            t1.setTextColor(Color.BLACK);
-            t1.setPadding(20,20,20,20);
-            tableRow.addView(t1);
-
-            t2 = new TextView(this);
-            t2.setText(cursor.getString(cursor.getColumnIndex("phoneNumber")));
-            t2.setTextColor(Color.BLACK);
-            t2.setPadding(20,20,20,20);
-            tableRow.addView(t2);
-
-            tableLayout.addView(tableRow);
-            cursor.moveToNext();
-        }
+//        for (int i = 0; i < count; i++) {
+//            Log.d("Display", "here");
+//
+//            tableRow = new TableRow(this);
+//            t0 = new TextView(this);
+//            t0.setText(cursor.getString(cursor.getColumnIndex("fName")));
+//            t0.setTextColor(Color.BLACK);
+//            t0.setPadding(20,20,20,20);
+//            tableRow.addView(t0);
+//
+//            t1 = new TextView(this);
+//            t1.setText(cursor.getString(cursor.getColumnIndex("lName")));
+//            t1.setTextColor(Color.BLACK);
+//            t1.setPadding(20,20,20,20);
+//            tableRow.addView(t1);
+//
+//            t2 = new TextView(this);
+//            t2.setText(cursor.getString(cursor.getColumnIndex("phoneNumber")));
+//            t2.setTextColor(Color.BLACK);
+//            t2.setPadding(20,20,20,20);
+//            tableRow.addView(t2);
+//
+//            tableLayout.addView(tableRow);
+//            cursor.moveToNext();
+//        }
 
     }
 }
